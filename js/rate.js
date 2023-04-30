e1c211018486e3fa18eb70744598c7cf3138164d
abrleva8 <abrleva8@gmail.com>
2023-05-01 01:43:33 +0300

    add delete rate

--- js/rate.js
+++ js/rate.js
@@ -1,6 +1,24 @@
-
+let rate = 0
 function rateClick(el) {
-    let rate = el.value
-    const x = $("#rate_text");
+    rate = el.value
+    const x = $("#rate_text")
+    const delete_button = $("#delete_button")
     x.html("Your rating: " + rate)
+    delete_button.css({'font-size':12})
+}
+
+function deleteClick(el) {
+
+    const x = $("#rate_text")
+    const delete_button = $("#delete_button")
+    x.html("Your rating: ")
+    delete_button.css({'font-size':0})
+
+    for (let i = 1; i <= rate; i++) {
+        let right_id = "#star" + i
+        let t = $(right_id)
+        t.prop("checked", false);
+    }
+
+    rate = 0
 }
