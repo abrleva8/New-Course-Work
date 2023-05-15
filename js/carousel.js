8ca0135b39422cf9ad29f6b6e2c0ed5bf5844b3c
abrleva8 <abrleva8@gmail.com>
2023-05-15 15:06:55 +0300

    fix open books

--- js/carousel.js
+++ js/carousel.js
@@ -4,20 +4,16 @@ var car_title = $("#carousel_title")
 var car_desc = $("#carousel_desc")
 var car_id = $(".id_book")
 
-var titles = [];
-var images = [];
-var descriptions = [];
-
-$(window).load(function () {
-    get_carousel_data()
-})
+var ids = []
+var titles = []
+var images = []
+var descriptions = []
 
 function get_carousel_data(){
     $.ajax({
         type: "GET",
         async: false,
-        url: "../php/get_carousel_info.php",
-        data: {id_book: localStorage.getItem("current_book")},
+        url: "../php/get_carousel_info.php"
     })
         .done(function (data) {
             if (data === "Ошибка!!!") {
@@ -29,29 +25,33 @@ function get_carousel_data(){
 }
 
 function set_lists(data, c='~') {
+    ids = []
     titles = []
     images = []
     descriptions = []
-    x = data.split(c)
+    let x = data.split(c)
     x.forEach((el) => {
         let current_row = el.split('#')
-        titles.push(current_row[0]);
-        descriptions.push(current_row[1]);
-        images.push(current_row[2]);
+        ids.push(current_row[0])
+        titles.push(current_row[1]);
+        descriptions.push(current_row[2]);
+        images.push(current_row[3]);
     })
 }
 
 function set_carousel($id){
+    if (images[$id] === undefined)
+        get_carousel_data()
     id_car = $id
     set_selector_colors()
     car_title.text(titles[$id])
     car_img.attr('src', images[$id])
-    car_id.attr('id', ($id + 1))
+    car_id.attr('id', (ids[$id]))
     car_desc.text(descriptions[$id])
     var selector = $("#selector" + $id)
     selector.attr("style","background: black")
 }
-var id_car = 0;
+var id_car= 0;
 
 function set_selector_colors(){
     for (let i = 0; i < 3; i++) {
@@ -60,14 +60,14 @@ function set_selector_colors(){
     }
 }
 
-function timer() {
+function set_carousel_timer() {
     set_carousel(id_car)
     if (id_car < 2)
         id_car++
     else id_car = 0
-    setTimeout(timer,3000)
+    setTimeout(set_carousel_timer,3000)
 }
 
-timer()
+set_carousel_timer()
 
 localStorage.setItem("name", "Левон")
--- php/get_carousel_info.php
+++ php/get_carousel_info.php
@@ -9,14 +9,15 @@ if ($link === false) {
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
     return;
 } else {
-    $sql = "SELECT book_name, book_description, book_image_source
+    $sql = "SELECT DISTINCT id_book, book_name, book_description, book_image_source
             FROM books
+            ORDER BY RAND()
             LIMIT 3";
     $stmt = $link->prepare($sql);
     if (mysqli_stmt_execute($stmt)) {
-        mysqli_stmt_bind_result($stmt, $col1, $col2, $col3);
+        mysqli_stmt_bind_result($stmt, $col1, $col2, $col3, $col4);
         while (mysqli_stmt_fetch($stmt)) {
-            printf("%s#%s#%s~", $col1, $col2, $col3);
+            printf("%s#%s#%s#%s~", $col1, $col2, $col3, $col4);
         }
     } else {
         printf("Ошибка!!!");
